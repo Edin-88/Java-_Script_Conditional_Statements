c161ecb9bb57f4016d434df2bb69d684d8e7290d
Your Name <selmanovic.edin1@gmail.com>
2023-01-23 16:53:37 +0100

    finished

---
+++ conditionnal_statements_2_3_/assets/js/main.js
@@ -0,0 +1,10 @@
+function differenz (number) {
+    let ergebnisDifferenz = number - 27;
+        if (ergebnisDifferenz > 27) {
+            console.log(ergebnisDifferenz * 2);
+        }
+}
+
+differenz(35);
+differenz(74);
+differenz(123);
